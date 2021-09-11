10257e69c66d24610f381b19a851a5a8e4317d20
Maxime Welcklen <maxime.welcklen@he-arc.ch>
2021-09-11 22:53:44 +0200

    add gift to models

--- back/phoenix/phoenixapp/models.py
+++ back/phoenix/phoenixapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Gift(models.Model):
+    """ The gift is a relationship between two users with a status (sent, received)"""
+    sender = models.ForeignKey(User)
+    recipient = models.ForeignKey(User)
+    sent = models.BooleanField()
+    received = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.sender} to {self.recipient}"
